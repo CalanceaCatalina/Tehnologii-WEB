002eeaac1ce75c79784142afec9b811e8aba68fb
CalanceaCatalina <catalina.calancea@isa.utm.md>
2020-12-19 17:18:04 +0200

    Updated unit test file

--- WebApplication1.Tests/UnitTest.cs
+++ WebApplication1.Tests/UnitTest.cs
@@ -20,6 +20,34 @@ namespace WebApplication1.Tests
         }
 
         [TestMethod]
+        public void Test2_GivenAandB_ExpectedResult()
+        {
+            //arrange - aranjezi toate datele ptu test
+            var a = 2;
+            var b = 2;
+
+            // act - insusi faci apel la functia care o testezi
+            double result = System.Math.Pow(a,b);
+
+            //assert - verifici rezultatul
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Test3_GivenAandB_ExpectedResult()
+        {
+            //arrange - aranjezi toate datele ptu test
+            var a = 100;
+            var b = 20;
+
+            // act - insusi faci apel la functia care o testezi
+            var result = a / b;
+
+            //assert - verifici rezultatul
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
         public void Meow_Returns_ExpectedString()
         {
             //arrange
